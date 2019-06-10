1d4009761a9802ffa22e3d091ab67c7a21f4771e
zyh <zhaoyuhui@youedata.com>
2019-06-10 18:17:33 +0800

    配置路由+安装scss

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="not-found-warp">
+        <p>404</p>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'notFound'
+}
+</script>
+<style lang="scss" scoped>
+.not-found-warp{
+    p{
+        font-size:120px;
+    }  
+}
+</style>
+
