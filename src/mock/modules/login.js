fa1700946b9b792a48e4196be376a56417df346b
zyh <zhaoyuhui@youedata.com>
2019-06-11 14:31:08 +0800

    完善登录流程之---3.修改 mock 接口

--- src/mock/modules/login.js
+++ src/mock/modules/login.js
@@ -3,7 +3,7 @@ export function login () {
     return {
       // isOpen: false,
       url: 'http://localhost:8081/login',
-      type: 'get',
+      type: 'post',
       data: {
         'msg': 'success',
         'code': 0,
