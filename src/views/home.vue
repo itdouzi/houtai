61dca2eb91c93af1c9b6aacc6b351c45f65de1c1
zyh <zhaoyuhui@youedata.com>
2019-06-11 10:17:47 +0800

    安装 axios

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        home
+        <h2>home</h2>
+        <el-button type="primary" @click="testAxios()">测试axios调用</el-button>
     </div>
 </template>
 <script>
+// import func from '../../vue-temp/vue-editor-bridge';
+import axios from 'axios'
 export default {
-    name: 'Login',
+    name: 'Home',
+    methods:{
+        testAxios(){
+            axios.get('http://localhost:8086').then(function(res){
+                alert(res.data)
+            }).catch(function(res){
+                alert(res)
+            });
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
