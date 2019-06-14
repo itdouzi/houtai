147b7c96725f409b38e83b917b1f6dede389222a
zhaoyuhui <zhaoyuhui@youedata.com>
2019-06-14 17:20:01 +0800

    开启 Chrome 远程调试端口

---
+++ src/views/Sys/Log.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="log-container">
+        <h2>日志管理</h2> 
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Sys/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="user-container">
+        <h2>用户管理</h2> 
+        <el-button type="primary" @click="tomenu()">跳转到菜单管理</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        tomenu(){
+            this.$router.push({path:'/sys/menu'})
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
